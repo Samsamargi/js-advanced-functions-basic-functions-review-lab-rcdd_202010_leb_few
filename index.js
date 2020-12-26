0458f5ef15d6af700ff1e818c04a4da7e088a2b1
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 15:38:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(){
+  console.log('This Saturday, I want to roller-skate!');
+}
+saturdayFun();
+
+saturdayFun();
+function saturdayFun(){
+  console.log('This Saturday, I want to bathe my dog!');
+}
+
+let fn=function(mondayWork){
+  console.log('This Monday, I will go to the office.');
+}
+fn()
+
+function greet(){
+  console.log('hello')
+}
+greet(outer)
